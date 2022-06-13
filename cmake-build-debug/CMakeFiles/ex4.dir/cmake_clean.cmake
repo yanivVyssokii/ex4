b8b8431f47ef86e111e02f8090b4c868ac6a61e6
Yaniv Vyssokii <yanivv@campus.technion.ac.il>
2022-06-13 17:36:03 +0300

    added directories

---
+++ cmake-build-debug/CMakeFiles/ex4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4.dir/main.cpp.obj"
+  "ex4.exe"
+  "ex4.exe.manifest"
+  "ex4.lib"
+  "ex4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
