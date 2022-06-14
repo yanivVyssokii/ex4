7e05664bbb64f81eb94dd1a2b7acc7c66765941e
Idan Hermelin <idanhermelin@campus.technion.ac.il>
2022-06-14 18:59:14 +0300

    Amir Khalif

---
+++ Cards/Merchant.cpp
@@ -0,0 +1,7 @@
+//
+// Created by idanh on 6/14/2022.
+//
+
+#include "Merchant.h"
+Merchant::Merchant(): Card(CardType::Merchant), m_HpPrice(5),m_ForcePrice(10)
+{}
---
+++ Cards/Merchant.h
@@ -0,0 +1,17 @@
+//
+// Created by idanh on 6/14/2022.
+//
+
+#ifndef EX4_MERCHANT_H
+#define EX4_MERCHANT_H
+
+#include "Card.h"
+class Merchant:public Card{
+public:
+    Merchant();
+
+private:
+    int m_HpPrice;
+    int m_ForcePrice;
+};
+#endif //EX4_MERCHANT_H
