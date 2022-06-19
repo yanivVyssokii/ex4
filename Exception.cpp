003f51bb3c1962c4b1ae0ba43f627eaab97add96
Yaniv Vyssokii <yanivv@campus.technion.ac.il>
2022-06-19 17:21:37 +0300

    finished exceptions(?)

--- Exception.cpp
+++ Exception.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Exception.h"
+#include <string>
+
+DeckFileNotFound::DeckFileNotFound(std::string fileName): std::invalid_argument("Deck File Error: File not found")
+{}
+
+const char *DeckFileNotFound::what() const {
+    return exception::what();
+}
+
+
+DeckFileInvalidSize::DeckFileInvalidSize(): std::length_error("Deck File Error: Deck size is invalid")
+{}
+
+const char *DeckFileInvalidSize::what() const {
+    return exception::what();
+}
+
+DeckFileFormatError::DeckFileFormatError(int line): m_line(line)
+{}
+
+const char *DeckFileFormatError::what() const {
+    std::string massage = std::string("Deck File Error: File format error in line ")+ std::to_string(m_line);
+    return massage.c_str();
+}
+
+
--- Exception.h
+++ Exception.h
@@ -5,4 +5,29 @@
 #ifndef EX4_EXCEPTION_H
 #define EX4_EXCEPTION_H
 
+#include <exception>
+#include <iostream>
+
+class DeckFileNotFound: public std::invalid_argument{
+public:
+    explicit DeckFileNotFound(std::string fileName);
+    const char* what() const override;
+
+};
+
+class DeckFileInvalidSize: public std::length_error{
+public:
+    explicit DeckFileInvalidSize();
+    const char* what() const override;
+
+};
+
+class DeckFileFormatError: public std::exception{
+public:
+    explicit DeckFileFormatError(int line);
+    const char* what() const override;
+
+private:
+    int m_line;
+};
 #endif //EX4_EXCEPTION_H
